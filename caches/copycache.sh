84d98f2975c115606a9a03fd351da11ce63ec2f8
exaithrg <syhaoran@163.com>
2024-12-15 00:39:53 +0800

    auto copycache.sh ok

--- caches/copycache.sh
+++ caches/copycache.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-cp -i ~/.cache/kdcache/stat/* .
+SOURCE_DIR=~/.cache/kdcache/stat
+TARGET_DIR=.
+
+# mkdir -p "$TARGET_DIR"
+
+current_hostname=$(hostname)
+current_datetime=$(date +"%Y_%m_%d_%H_%M_%S")
+
+for file in "$SOURCE_DIR"/*.json; do
+  if [ -e "$file" ]; then
+    base_name=$(basename "$file" .json)
+    new_file_name="${base_name}_${current_hostname}_${current_datetime}.json"
+    cp -i "$file" "$TARGET_DIR/$new_file_name"
+  else
+    echo "No .json exists"
+  fi
+done
+
+echo "Copy kdcache json completed"
